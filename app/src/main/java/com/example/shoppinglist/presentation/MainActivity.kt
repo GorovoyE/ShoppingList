e2f9c9feefce3de34f1993067220fa9c7873100d
Evgeniy Gorovoy <kaknazvattebya@gmail.com>
2024-01-08 20:24:00 +0700

    delete unused imports

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.shoppinglist.presentation
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
