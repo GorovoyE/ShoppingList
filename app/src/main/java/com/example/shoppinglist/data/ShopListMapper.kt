8263e6f2cae78d0bc64f49ca9b7f81b293656f8d
Evgeniy Gorovoy <kaknazvattebya@gmail.com>
2024-01-10 12:17:29 +0700

    add db, mapper, coroutines, update target and compile sdk

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListMapper.kt
@@ -0,0 +1,29 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopListMapper {
+
+    fun mapEntityToDbModel(shopItem: ShopItem): ShopItemDbModel {
+        return ShopItemDbModel(
+            id = shopItem.id,
+            name = shopItem.name,
+            count = shopItem.count,
+            enabled = shopItem.enabled
+        )
+    }
+
+    fun mapDmModelToEntity(shopItemDbModel: ShopItemDbModel): ShopItem {
+        return ShopItem(
+            id = shopItemDbModel.id,
+            name = shopItemDbModel.name,
+            count = shopItemDbModel.count,
+            enabled = shopItemDbModel.enabled
+        )
+    }
+
+    fun mapListDbModelToListEntity(list: List<ShopItemDbModel>) = list.map {
+        mapDmModelToEntity(it)
+    }
+
+}
