d6c01326f54396d5b2063525c9e9eb4164a89b2a
Evgeniy Gorovoy <kaknazvattebya@gmail.com>
2024-01-29 17:16:49 +0700

    add di

---
+++ app/src/main/java/com/example/shoppinglist/di/DataModule.kt
@@ -0,0 +1,29 @@
+package com.example.shoppinglist.di
+
+import android.app.Application
+import com.example.shoppinglist.data.AppDatabase
+import com.example.shoppinglist.data.ShopListDao
+import com.example.shoppinglist.data.repository.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.ShopListRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module
+interface DataModule {
+
+    @ApplicationScope
+    @Binds
+    fun bindShoppingListRepository(impl: ShopListRepositoryImpl): ShopListRepository
+
+    companion object {
+
+        @ApplicationScope
+        @Provides
+        fun provideShopListDao(
+            application: Application
+        ): ShopListDao {
+            return AppDatabase.getInstance(application).shopListDao()
+        }
+    }
+}
